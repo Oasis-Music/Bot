73a0ee92f05536aa8653f151141b056c9f13794f
Vlad-Shevliakov <vladislavs319@gmail.com>
2022-08-05 17:16:07 +0300

    new shared component: SvgIcon

---
+++ client/src/shared/SvgIcon/index.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  font-size: 1em;
+  width: 1.5em;
+  height: 1.5em;
+  user-select: none;
+  display: inline-flex;
+  justify-content: center;
+  fill: currentColor;
+  flex-shrink: 0;
+  transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  -webkit-transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  -ms-flex-negative: 0;
+  -webkit-flex-shrink: 0;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+`
