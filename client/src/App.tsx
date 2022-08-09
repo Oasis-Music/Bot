39090e2dc2800856ac445ca4ec52c47fc26d9002
Vlad-Shevliakov <vladislavs319@gmail.com>
2022-08-09 18:54:20 +0300

    app wrapped on AppLayout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,24 @@
 import React, { lazy, Suspense } from 'react'
-// import AppLayout from './components/AppLayout/AppLayout'
+import AppLayout from './components/AppLayout/AppLayout'
 import Fallback from './components/Fallback/Fallback'
 import PrivateRoute from './shared/PrivateRoute'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
-const Player = lazy(() => import('./pages/Player'))
+const Home = lazy(() => import('./pages/Home'))
 const UI = lazy(() => import('./pages/UI'))
 
 const App: React.FC = () => {
   return (
     <Suspense fallback={<Fallback />}>
-      {/* <AppLayout> */}
       <Routes>
-        {/* <Route path="/auth" element={<Auth />} /> */}
-        <Route element={<PrivateRoute />}>
-          <Route path="/player" element={<Player />} />
-          <Route path="/ui" element={<UI />} />
+        <Route path="/" element={<AppLayout />}>
+          <Route element={<PrivateRoute />}>
+            <Route index element={<Home />} />
+            <Route path="/ui" element={<UI />} />
+          </Route>
         </Route>
-        <Route path="*" element={<Navigate to="/orders" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-      {/* </AppLayout> */}
     </Suspense>
   )
 }
