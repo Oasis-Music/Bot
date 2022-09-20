9aee5911e813bc944c9a45b125b621564b9c4abd
Vlad-Shevliakov <vladislavs319@gmail.com>
2022-09-20 20:28:32 +0300

    navigation: controls update

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 import SvgIcon from '../../shared/SvgIcon'
 import { Container, SearchButton } from './Nav.styled'
 import { ReactComponent as SearchIcon } from '../../assets/svg/search.svg'
+import { ReactComponent as MusicListIcon } from '../../assets/svg/list-music.svg'
+import { ReactComponent as CogIcon } from '../../assets/svg/cog.svg'
+
 import history from '../../utils/history'
 
 const Nav: React.FC = () => {
   const searchClickHandler = () => {
+    history.push('/search')
+  }
+
+  const playlistClickHandler = () => {
+    history.push('/')
+  }
+
+  const cogClickHandler = () => {
     history.push('/test')
   }
 
@@ -14,6 +25,16 @@ const Nav: React.FC = () => {
       <SearchButton onClick={searchClickHandler}>
         <SvgIcon>
           <SearchIcon />
+        </SvgIcon>
+      </SearchButton>
+      <SearchButton onClick={playlistClickHandler}>
+        <SvgIcon>
+          <MusicListIcon />
+        </SvgIcon>
+      </SearchButton>
+      <SearchButton onClick={cogClickHandler}>
+        <SvgIcon>
+          <CogIcon />
         </SvgIcon>
       </SearchButton>
     </Container>
