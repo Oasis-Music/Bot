8df613689a51101738eb2196638b81eed30c280c
Vlad-Shevliakov <vladislavs319@gmail.com>
2022-10-09 20:23:34 +0200

    save new track and cover on external service

--- backend/internal/domain/services/soundtrack/addSoundtrack.go
+++ backend/internal/domain/services/soundtrack/addSoundtrack.go
@@ -5,13 +5,19 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"math"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"os/exec"
 
 	"oasis/backend/internal/adapters/db"
 	"oasis/backend/internal/adapters/graph/models"
+
+	"github.com/99designs/gqlgen/graphql"
 )
 
 type Tags map[string]interface{}
@@ -32,6 +38,13 @@ type Format struct {
 	BitRate          string  `json:"bit_rate"`
 	ProbeScore       int     `json:"probe_score"`
 	TagList          Tags    `json:"tags"`
+}
+
+func trackDurationToInt16(d float64) (int16, error) {
+	if d <= 0 {
+		return 0, errors.New("track duration is 0")
+	}
+	return int16(math.Round(d)), nil
 }
 
 func (s *soundtrackService) AddSoundtrack(ctx context.Context, input models.AddSoundtrackInput) (bool, error) {
@@ -81,10 +94,10 @@ func (s *soundtrackService) AddSoundtrack(ctx context.Context, input models.AddS
 
 	tempFile.Write(resultBuffer.Bytes())
 
-	fmt.Println("ffprobe open: ", tempFile.Name())
+	fileName := tempFile.Name()
 
 	// ffprobe -v quiet -print_format json -show_format -
-	probe := exec.Command("ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", tempFile.Name())
+	probe := exec.Command("ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", fileName)
 
 	var ffprobeBuf bytes.Buffer
 
@@ -109,35 +122,30 @@ func (s *soundtrackService) AddSoundtrack(ctx context.Context, input models.AddS
 
 	fmt.Printf("Final info --> FormatName: %s, Duration: %v\n", data.Format.FormatName, data.Format.DurationSeconds)
 
-	defer os.Remove(tempFile.Name())
+	defer os.Remove(fileName)
 
-	/*
-		title: String!
-		author: String!
-		coverImage: Upload!
-		file: Upload!
+	duration, err := trackDurationToInt16(data.Format.DurationSeconds)
+	if err != nil {
+		return false, err
+	}
 
-		-------------------
-
-		Title       string +
-		Author      string +
-		Duration    int16
-		CoverImage  sql.NullString
-		FileURL     string
-		IsValidated bool
-		CreatorID   string
-	*/
+	soundtrackURL, coverImageURL, err := s.saveMediaOnLocalServer(resultBuffer, input.CoverImage)
+	if err != nil {
+		return false, err
+	}
 
 	var dbParams db.NewSoundtrackParams
 
 	dbParams.Title = input.Title
 	dbParams.Author = input.Author
+	dbParams.Duration = duration
 
 	// TODO: fill
-
-	dbParams.Duration = int16(100)
-	dbParams.CoverImage = sql.NullString{}
-	dbParams.FileURL = "uknown"
+	dbParams.CoverImage = sql.NullString{
+		Valid:  true,
+		String: coverImageURL,
+	}
+	dbParams.FileURL = soundtrackURL
 	dbParams.IsValidated = false
 	dbParams.CreatorID = "sys"
 
@@ -149,4 +157,76 @@ func (s *soundtrackService) AddSoundtrack(ctx context.Context, input models.AddS
 	fmt.Println("Got ID:", id)
 
 	return true, nil
+}
+
+func (s *soundtrackService) saveMediaOnLocalServer(audio *bytes.Buffer, coverImage graphql.Upload) (string, string, error) {
+
+	var body bytes.Buffer
+	writer := multipart.NewWriter(&body)
+
+	audioField, err := writer.CreateFormFile("soundtrack", "new_track.mp3")
+	if err != nil {
+		fmt.Println(err)
+		return "", "", errors.New("failed to save media")
+
+	}
+	audioField.Write(audio.Bytes())
+
+	coverImg, err := io.ReadAll(coverImage.File)
+	if err != nil {
+		fmt.Println("io.ReadAll cover:", err)
+		return "", "", errors.New("failed to save media")
+	}
+
+	coverImageField, err := writer.CreateFormFile("cover", coverImage.Filename)
+	if err != nil {
+		fmt.Println(err)
+		return "", "", errors.New("failed to save media")
+
+	}
+	coverImageField.Write(coverImg)
+
+	if err := writer.Close(); err != nil {
+		fmt.Println(err)
+		return "", "", errors.New("failed to save media")
+	}
+
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:5000/createTrack", &body)
+	if err != nil {
+		fmt.Println(err)
+		return "", "", errors.New("failed to save media")
+
+	}
+
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return "", "", errors.New("failed to save media")
+
+	}
+
+	respBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return "", "", errors.New("failed to save media")
+
+	}
+
+	defer res.Body.Close()
+
+	type responseData struct {
+		AudioPath      string `json:"audioPath"`
+		CoverImagePath string `json:"coverPath"`
+	}
+
+	var data responseData
+
+	if err := json.Unmarshal(respBody, &data); err != nil {
+		fmt.Println(err)
+		return "", "", errors.New("failed to save media")
+	}
+
+	return data.AudioPath, data.CoverImagePath, nil
 }
