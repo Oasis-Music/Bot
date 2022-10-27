a6331a133ffa51e6a963be955848a592bfb4c51d
Vlad-Shevliakov <vladislavs319@gmail.com>
2022-10-27 18:20:11 +0300

    "Cover" step finally styled

--- client/src/pages/Upload/Cover/Cover.styled.ts
+++ client/src/pages/Upload/Cover/Cover.styled.ts
@@ -4,27 +4,29 @@ import SvgIcon from '../../../shared/SvgIcon'
 
 export const Container = styled.div`
   color: #fff;
-  width: 100%;
-  height: 100vh;
   box-sizing: border-box;
   padding: 20px 10px 0 10px;
 `
 
 export const StepTitle = styled.h2`
   margin: 0;
-  margin-left: 10px;
-  margin-bottom: 20px;
+  font-size: 4.5vh;
   text-align: start;
+  margin-left: 10px;
+  margin-bottom: 1.5vh;
 `
 
 export const Title = styled.p`
-  color: #aaaaaa;
+  font-size: 2.6vh;
+  color: #ababab;
   margin: 0;
   margin-left: 10px;
-  margin-bottom: 20px;
+  margin-bottom: 5vh;
 `
 
 interface containerStyles {
+  $droped: boolean
+  isError: boolean
   dragActive: boolean
   isDragAccept: boolean
   isDragReject: boolean
@@ -40,59 +42,63 @@ const getColor = (props: containerStyles) => {
   if (props.isDragReject) {
     return '#ff1744'
   }
+  if (props.isError) {
+    return '#ff1744'
+  }
 
   return '#eeeeee'
 }
 
 export const ContainerUpload = styled.div<containerStyles>`
-  box-sizing: border-box;
   position: relative;
-  border: 2px dashed;
+  border-width: 2px;
+  border-style: ${({ $droped }) => ($droped ? 'solid' : 'dashed')};
   border-color: #b5b5b5;
   border-radius: 8px;
-  width: 100%;
-  height: 100%;
-  max-width: 240px;
-  max-height: 240px;
+  width: 41vh;
+  aspect-ratio: 1 / 1;
   margin: 0 auto;
   outline: none;
   transition: border 0.3s;
   border-color: ${(props) => getColor(props)};
   margin: 0 auto;
-  margin-bottom: 20px;
 `
 
 export const Preview = styled.img`
   display: block;
   width: inherit;
-  height: inherit;
+  height: 100%;
+  border-radius: 8px;
 `
 
 export const Plug = styled.div`
   color: #fff;
-  /* background-color: #fff; */
-  /* stroke: none; */
   height: 100%;
   display: flex;
   justify-content: center;
   flex-direction: column;
-  font-size: 60px;
   align-items: center;
   border-radius: 8px;
+  margin-bottom: 7px;
 `
 
 export const PlugIcon = styled(SvgIcon)`
-  /* font-size: 60px; */
+  font-size: 9vh;
 `
 
 export const PlugInfo = styled.div`
-  font-size: 14px;
   & > p {
+    font-size: 2.7vh;
     padding: 0 5px;
     text-align: center;
+    margin-bottom: 8px;
   }
   & > ul {
+    font-size: 2.3vh;
+    color: #b5b5b5;
     list-style: disc;
+    display: table;
+    margin: 0 auto;
   }
 `
 
@@ -110,4 +116,16 @@ export const DeleteButton = styled(IconButton)`
   &:hover:not(:disabled) {
     background-color: #fff;
   }
+`
+
+export const ErrorMessage = styled.p`
+  color: #ff182e;
+  text-align: center;
+  font-size: 14px;
+  line-height: 14px;
+  margin: 0;
+  margin-top: 10px;
+  height: 13px;
+  transition: all 0.3s linear;
+  margin-bottom: 5vh;
 `
--- client/src/pages/Upload/Cover/Cover.tsx
+++ client/src/pages/Upload/Cover/Cover.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react'
-// import clsx from 'clsx'
-import Button from '../../../shared/Button'
-
-import { useDropzone } from 'react-dropzone'
+import { useSpring, animated } from 'react-spring'
+import { useDropzone, FileRejection } from 'react-dropzone'
 import { ReactComponent as DeleteIcon } from '../../../assets/svg/trash.svg'
-import SvgIcon from '../../../shared/SvgIcon'
 import { ReactComponent as CoverPlaceholderIcon } from '../../../assets/svg/cover_placeholder.svg'
 import {
   Container,
@@ -15,22 +12,39 @@ import {
   Plug,
   PlugIcon,
   PlugInfo,
+  ErrorMessage,
   DeleteButton
 } from './Cover.styled'
 import { useFormikContext } from 'formik'
-
-interface UploadedFile extends File {
-  preview: string
-}
+import SvgIcon from '../../../shared/SvgIcon'
+import StepControls from '../StepControls'
 
 interface CoverProps {
   onNextStep(): void
   onPrevStep(): void
 }
 
+const MAX_COVER_SIZE = 700
+
+interface UploadedFile extends File {
+  preview: string
+}
+
+function dropzoneCodeToMsg(code: string): string {
+  switch (code) {
+    case 'file-invalid-type':
+      return '* тип файла только: .jpeg, .jpg'
+    case 'file-too-large':
+      return `* файл весит больше ${MAX_COVER_SIZE} KB`
+    default:
+      return '* что-то пошло не так'
+  }
+}
+
 const Cover: React.FC<CoverProps> = ({ onNextStep, onPrevStep }) => {
-  const { setFieldValue, errors } = useFormikContext()
+  const { setFieldValue } = useFormikContext()
   const [mainPhoto, setMainPhoto] = useState<UploadedFile>()
+  const [dropError, setDropError] = useState<string>('')
 
   useEffect(() => {
     return () => {
@@ -40,39 +54,58 @@ const Cover: React.FC<CoverProps> = ({ onNextStep, onPrevStep }) => {
     }
   }, [])
 
+  const fadeStyles = useSpring({
+    config: { duration: 250 },
+    from: { opacity: 0 },
+    to: {
+      opacity: dropError ? 1 : 0
+    }
+  })
+
   const handleFileDrop = (acceptedFiles: globalThis.File[]) => {
     if (mainPhoto) URL.revokeObjectURL(mainPhoto.preview)
 
     if (acceptedFiles.length) {
-      const uploaded: unknown = acceptedFiles[0] // Type assertions
-      const file = uploaded as UploadedFile
+      if (dropError) setDropError('')
 
-      const s: unknown = file
+      const file = acceptedFiles[0] as UploadedFile
+      file.preview = URL.createObjectURL(file)
 
-      file.preview = URL.createObjectURL(s as Blob)
+      const image = new Image()
+      image.src = file.preview
+      image.onload = () => {
+        const w = image.width,
+          h = image.height
 
-      //   onMainPhotoUpload(file)
+        if (w !== h) {
+          setDropError('* ширина и высота должны быть одинаковы')
+          return
+        }
+
+        if (w > 1000) {
+          setDropError('* разрешение больше чем 1000×1000 px')
+        }
+      }
 
       setMainPhoto(file)
       setFieldValue('coverImage', file)
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const handleDropReject = (rejectedFiles: any) => {
-    console.log(rejectedFiles)
-    const { errors } = rejectedFiles[0]
+  const handleDropReject = (fileRejections: FileRejection[]) => {
+    console.log(fileRejections)
+    const { errors } = fileRejections[0]
+    console.log('e', errors)
     const { code } = errors[0]
-    // code: "file-too-large"
     console.log(code)
-    // TODO custom error on drop rejected
+    setDropError(dropzoneCodeToMsg(code))
   }
 
   const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
     accept: {
       'image/jpeg': ['.jpg']
     },
-    maxSize: 716800, // 700KB
+    maxSize: 716800,
     maxFiles: 1,
     multiple: false,
     onDrop: handleFileDrop,
@@ -81,10 +114,10 @@ const Cover: React.FC<CoverProps> = ({ onNextStep, onPrevStep }) => {
 
   const handleRemoveClick = (event: React.MouseEvent<HTMLElement>): void => {
     event.stopPropagation()
+    setDropError('')
 
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     URL.revokeObjectURL(mainPhoto!.preview)
-    // onMainPhotoUpload(null)
     setMainPhoto(undefined)
     setFieldValue('coverImage', null)
   }
@@ -92,7 +125,7 @@ const Cover: React.FC<CoverProps> = ({ onNextStep, onPrevStep }) => {
   return (
     <Container>
       <StepTitle>Шаг #2</StepTitle>
-      <Title>Добавить изображение обложки</Title>
+      <Title>Добавь изображение обложки</Title>
       <ContainerUpload
         {...{
           isDragActive,
@@ -100,26 +133,31 @@ const Cover: React.FC<CoverProps> = ({ onNextStep, onPrevStep }) => {
           isDragReject
         }}
         {...getRootProps()}
+        isError={dropError}
+        $droped={!!mainPhoto?.size}
       >
         <input {...getInputProps()} />
         {mainPhoto ? (
-          <Preview src={mainPhoto?.preview} alt="Загруженное фото создаваемого товара" />
+          <Preview
+            src={mainPhoto.preview}
+            alt="Загруженное обложки трека"
+            onLoad={() => URL.revokeObjectURL(mainPhoto.preview)}
+          />
         ) : (
           <Plug>
             <PlugIcon>
               <CoverPlaceholderIcon />
             </PlugIcon>
             <PlugInfo>
-              <p>Перетащи файл сюда или нажмите на область</p>
+              <p>Перетащи файл сюда или нажми на область</p>
               <ul>
-                <li>Не более 1 mb</li>
-                <li>jpeg или jpg</li>
-                <li>До 1000px&#215;1000px</li>
+                <li>Не более&nbsp;{MAX_COVER_SIZE}&nbsp;KB</li>
+                <li>Формат JPEG</li>
+                <li>До 1000&#215;1000 px</li>
               </ul>
             </PlugInfo>
           </Plug>
         )}
-        {isDragReject && <span>Типом файла может быть только: .jpeg, .jpg</span>}
         {mainPhoto && (
           <DeleteButton withoutShadow onClick={handleRemoveClick}>
             <SvgIcon>
@@ -128,19 +166,16 @@ const Cover: React.FC<CoverProps> = ({ onNextStep, onPrevStep }) => {
           </DeleteButton>
         )}
       </ContainerUpload>
-      <Button tabIndex={-1} disableShadow fullWidth onClick={onPrevStep}>
-        Prev
-      </Button>
-      <Button
-        tabIndex={-1}
-        disableShadow
-        disabled={!!!mainPhoto?.name}
-        fullWidth
-        onClick={onNextStep}
-      >
-        Next
-      </Button>
-      <div color="white">{JSON.stringify(errors)}</div>
+      <animated.div style={fadeStyles}>
+        <ErrorMessage>{dropError}</ErrorMessage>
+      </animated.div>
+      <StepControls
+        disabled={!!!mainPhoto?.size || !!dropError}
+        nextText="Продолжить"
+        onBack={onPrevStep}
+        onNext={onNextStep}
+      />
+      {/* <div color="white">{JSON.stringify(errors)}</div> */}
     </Container>
   )
 }
