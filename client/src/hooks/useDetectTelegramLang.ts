0103d29fe0b441c01da3fd5d9d8f8a3b3c8f8147
fresonn <vladislavs319@gmail.com>
2023-02-03 18:00:14 +0200

    c: useDetectTelegramLang hook try-catch wrapper

--- client/src/hooks/useDetectTelegramLang.ts
+++ client/src/hooks/useDetectTelegramLang.ts
@@ -6,13 +6,19 @@ export const useDetectTelegramLang = () => {
   const { i18n } = useTranslation()
 
   useEffect(() => {
-    const tgLang = Telegram.WebApp.initDataUnsafe.user?.language_code || 'en'
+    try {
+      const tgLang = Telegram.WebApp.initDataUnsafe.user?.language_code || 'en'
 
-    if (['en', 'ru'].includes(tgLang)) {
-      i18n.changeLanguage(tgLang)
-      return
+      if (['en', 'ru'].includes(tgLang)) {
+        i18n.changeLanguage(tgLang)
+        return
+      }
+
+      i18n.changeLanguage('en')
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message)
+      }
     }
-
-    i18n.changeLanguage('en')
   }, [])
 }
