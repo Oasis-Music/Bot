7732b4d1d1ff93c3879a8d3392c318dc7b58b04d
fresonn <vladislavs319@gmail.com>
2023-02-14 23:48:38 +0200

    c: i18next debug mode disabled by default

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -6,7 +6,7 @@ i18n
   .use(Backend)
   .use(initReactI18next)
   .init<HttpBackendOptions>({
-    debug: process.env.NODE_ENV === 'development',
+    debug: false,
     fallbackLng: 'en',
     ns: ['translation'],
     defaultNS: 'translation',
