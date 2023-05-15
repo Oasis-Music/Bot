5b4fdd8000861b72ae6db67faf6bc584eb9e31f0
fresonn <vladislavs319@gmail.com>
2023-05-15 20:42:39 +0300

    c: audio upload step plug icon styles - fix

--- client/src/pages/Upload/Audio/Dropzone/Dropzone.tsx
+++ client/src/pages/Upload/Audio/Dropzone/Dropzone.tsx
@@ -2,9 +2,17 @@ import React, { useState } from 'react'
 import { useDropzone, FileRejection } from 'react-dropzone'
 import { ReactComponent as AudioPlaceholderIcon } from '../../../../assets/svg/audio_placeholder.svg'
 import { ReactComponent as CheckIcon } from '../../../../assets/svg/check-circle.svg'
-import { ContainerUpload, Plug, PlugIcon, PlugInfo, ErrorMessage } from './Dropzone.styled'
 import { useTranslation } from 'react-i18next'
 import AudioPlayer from '../../../../player'
+
+import {
+  ContainerUpload,
+  Plug,
+  PlugIconBox,
+  PlugIcon,
+  PlugInfo,
+  ErrorMessage
+} from './Dropzone.styled'
 interface DropzoneProps {
   audio: File | null
   player: AudioPlayer | undefined
@@ -96,7 +104,9 @@ const Dropzone: React.FC<DropzoneProps> = ({ audio, player, onStop, onSetAudio, 
       >
         <input {...getInputProps()} />
         <Plug>
-          <PlugIcon>{audio?.size ? <CheckIcon /> : <AudioPlaceholderIcon />}</PlugIcon>
+          <PlugIconBox>
+            <PlugIcon>{audio?.size ? <CheckIcon /> : <AudioPlaceholderIcon />}</PlugIcon>
+          </PlugIconBox>
           <PlugInfo>
             <p>{t('pages.upload.audio.dropzone.title')}</p>
             <ul>
