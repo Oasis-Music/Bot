6c91333d913156512de27ff28f60b03010213975
fresonn <vladislavs319@gmail.com>
2023-08-18 18:43:14 +0300

    c: new CHECKBOX form field

--- client/src/shared/FormFields/Checkbox/index.tsx
+++ client/src/shared/FormFields/Checkbox/index.tsx
@@ -1,112 +1,108 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes, css } from 'styled-components'
 import { useField } from 'formik'
 
 interface CheckboxProps {
   name: string
+  value?: string
   checked?: boolean
   label?: string
   onChange?(event: React.ChangeEvent<HTMLInputElement>): void
 }
 
-const Wrapper = styled.span`
-  display: -webkit-inline-box;
-  display: -webkit-inline-flex;
-  display: -ms-inline-flexbox;
-  display: inline-flex;
-  -webkit-align-items: center;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  -webkit-justify-content: center;
-  justify-content: center;
-  position: relative;
-  box-sizing: border-box;
-  -webkit-tap-highlight-color: transparent;
-  background-color: transparent;
-  outline: 0;
-  border: 0;
-  margin: 0;
-  border-radius: 0;
-  padding: 0;
-  cursor: pointer;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  vertical-align: middle;
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  -webkit-text-decoration: none;
-  text-decoration: none;
-  color: inherit;
-  padding: 9px;
-  border-radius: 50%;
+const wave = keyframes`
+  50% {
+    transform: scale(0.9);
+  }
 `
 
-const BaseCheckbox = styled.input`
-  cursor: inherit;
-  position: absolute;
-  opacity: 0;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  margin: 0;
-  padding: 0;
-  z-index: 1;
-`
-
-const Icon = styled.svg<{ $checked: boolean }>`
-  font-size: 2rem;
-  user-select: none;
-  width: 1em;
-  height: 1em;
-  display: inline-block;
-  fill: ${({ $checked, theme }) => ($checked ? theme.colors.primary : '#868686')};
-  flex-shrink: 0;
-  transition: fill 200ms ease;
-  -webkit-flex-shrink: 0;
-  -ms-flex-negative: 0;
-  -webkit-transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-`
-
-const Label = styled.label`
+const LabelText = styled.span`
   color: inherit;
   font-size: inherit;
   font-weight: 500;
-  padding-left: 5px;
+  padding-left: 11px;
 `
 
-const uncheckedPath = (
-  <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z" />
-)
+const Input = styled.input`
+  position: absolute;
+  visibility: hidden;
+  display: none;
+`
 
-const checkedPath = (
-  <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
-)
+const Box = styled.span<{ $checked: boolean }>`
+  position: relative;
+  display: block;
+  width: 24px;
+  height: 24px;
+  border-radius: 4px;
+  transform: scale(1);
+  border: 2px solid #dcdcdc;
+  transition: all 0.2s ease;
+  box-shadow: 0 1px 1px rgba(0, 16, 75, 0.05);
 
-const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange, label, ...restProps }) => {
-  const [field] = useField(restProps)
+  & svg {
+    position: absolute;
+    top: 4px;
+    left: 3px;
+    fill: none;
+    stroke: #101318;
+    stroke-width: 2;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-dasharray: 16px;
+    stroke-dashoffset: 16px;
+    transition: all 0.3s ease;
+    transition-delay: 0.1s;
+    transform: translate3d(0, 0, 0);
+  }
+  ${({ $checked }) =>
+    $checked &&
+    css`
+      background: ${({ theme }) => theme.colors.primary};
+      border-color: ${({ theme }) => theme.colors.primary};
+      animation: ${wave} 0.4s ease;
+      & svg {
+        stroke-dashoffset: 0;
+      }
+    `};
+`
+
+const Label = styled.label`
+  display: inline-flex;
+  align-items: center;
+  overflow: hidden;
+  user-select: none;
+  transition: all 0.2s ease;
+  padding: 5px 8px;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    & > span {
+      border-color: ${({ theme }) => theme.colors.primary};
+    }
+  }
+`
+
+const Checkbox: React.FC<CheckboxProps> = ({ name, value, label, onChange, checked = false }) => {
+  const [field] = useField({ name, value, type: 'checkbox' })
+
+  const isChecked = field.checked || checked
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (onChange) onChange(event)
     field.onChange(event)
+    if (onChange) onChange(event)
   }
 
   return (
-    <Wrapper>
-      <BaseCheckbox type="checkbox" {...field} {...restProps} onChange={handleChange} />
-      <Icon $checked={field.value} focusable="false" viewBox="0 0 24 24">
-        {field.value ? checkedPath : uncheckedPath}
-      </Icon>
-      {label && <Label>{label}</Label>}
-    </Wrapper>
+    <Label>
+      <Input type="checkbox" {...field} onChange={handleChange} />
+      <Box $checked={isChecked}>
+        <svg width="14px" height="12px" viewBox="0 0 12 10">
+          <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+        </svg>
+      </Box>
+      {label && <LabelText>{label}</LabelText>}
+    </Label>
   )
 }
 
