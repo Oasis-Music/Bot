849ac16d025f2f4fa4627612705c61448b179508
fresonn <vladislavs319@gmail.com>
2024-02-16 16:20:55 +0200

    client: install sass

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default ({ mode }: { mode: boolean }) => {
   return defineConfig({
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/main.scss" as m;`
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
     plugins: [react(), svgr(), viteTsconfigPaths()],
     server: {
       port: 3001
