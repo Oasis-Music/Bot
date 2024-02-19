88ee51864f0b31620577e6e5c01ffef948bb13d0
fresonn <vladislavs319@gmail.com>
2024-02-19 14:20:31 +0200

    client: styled-components wiped

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,31 +1,26 @@
 import React from 'react'
-import theme, { GlobalStyles } from './utils/theme'
 import client from '@/apollo/apollo'
 import { useDetectLang } from '@/hooks'
 import { createRoot } from 'react-dom/client'
 import { ApolloProvider } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
-import { ModalProvider } from 'styled-react-modal'
 import { App } from './App'
+import Modal from 'react-modal'
 
 import 'normalize.css'
 
 import './i18n'
+
+Modal.setAppElement('#root')
 
 function Application() {
   useDetectLang()
 
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <ModalProvider>
-          <ApolloProvider client={client}>
-            <GlobalStyles />
-            <App />
-          </ApolloProvider>
-        </ModalProvider>
-      </ThemeProvider>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   )
 }
--- client/src/pages/Upload/modals/Alert/index.tsx
+++ client/src/pages/Upload/modals/Alert/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Modal from 'styled-react-modal'
 import Button from '@/components/ui/Button'
 import { useTranslation } from 'react-i18next'
-import { ITheme } from '@/utils/theme'
+import { Modal } from '@/components/Modal'
 
 import styles from './Alert.module.scss'
 import clsx from 'clsx'
@@ -17,33 +16,19 @@ export function Alert({ isOpen, onLeave, onStay }: AlertProps) {
   const { t } = useTranslation()
 
   return (
-    <StyledModal isOpen={isOpen}>
-      <h4 className={styles.title}>{t('pages.upload.modals.alert.title')}</h4>
-      <p className={styles.subTitle}>{t('pages.upload.modals.alert.message')}</p>
-      <div className={styles.inner}>
-        <Button onClick={onLeave} className={clsx(styles.actionButton, styles.leaveButton)}>
-          {t('pages.upload.modals.alert.yes')}
-        </Button>
-        <Button fullWidth color="secondary" onClick={onStay} className={styles.actionButton}>
-          {t('pages.upload.modals.alert.stay')}
-        </Button>
+    <Modal open={isOpen}>
+      <div className={styles.container}>
+        <h4 className={styles.title}>{t('pages.upload.modals.alert.title')}</h4>
+        <p className={styles.subTitle}>{t('pages.upload.modals.alert.message')}</p>
+        <div className={styles.inner}>
+          <Button onClick={onLeave} className={clsx(styles.actionButton, styles.leaveButton)}>
+            {t('pages.upload.modals.alert.yes')}
+          </Button>
+          <Button fullWidth color="secondary" onClick={onStay} className={styles.actionButton}>
+            {t('pages.upload.modals.alert.stay')}
+          </Button>
+        </div>
       </div>
-    </StyledModal>
+    </Modal>
   )
 }
-
-interface styledReactModalProps {
-  theme: ITheme
-}
-
-const StyledModal = Modal.styled`
-  width: 70vh;
-  padding-bottom: 5vh;
-  color: #fff;
-  border-radius: 10px;
-  background-color:#343434;
-  transition : all 0.3s ease-in-out;
-  @media ${(props: styledReactModalProps) => props.theme.media.hsm} {
-    width: 90%;
-  }
-`
