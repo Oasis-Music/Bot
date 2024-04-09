87ff535e1b7ca316dfdfa641cce6b7fb023ecb83
fresonn <vladislavs319@gmail.com>
2024-04-09 17:09:36 +0300

    client: copy-info soundtrack feature

--- client/src/pages/home/ui/head/index.tsx
+++ client/src/pages/home/ui/head/index.tsx
@@ -1,8 +1,5 @@
 import clsx from 'clsx'
-import { useEffect, useState } from 'react'
 import ShareIcon from '@/shared/assets/svg/share.svg?react'
-import CopyIcon from '@/shared/assets/svg/copy.svg?react'
-import CheckIcon from '@/shared/assets/svg/check-circle.svg?react'
 import { SvgIcon } from '@/shared/ui/svg-icon'
 import { IconButton } from '@/shared/ui/icon-button'
 import { ImagePlaceholder } from '@/shared/ui/image-placeholder'
@@ -10,6 +7,7 @@ import { useReactiveVar } from '@apollo/client'
 import { currentTrackVar } from '@/entities/soundtrack'
 import { AttachButton } from '@/features/soundtrack/add-to-user-playlist'
 import { UnattachButton } from '@/features/soundtrack/remove-from-user-playlist'
+import { CopyInfoButton } from '@/features/soundtrack/copy-info'
 
 import styles from './styles.module.scss'
 
@@ -20,33 +18,7 @@ interface HeadProps {
 }
 
 export function Head({ onTrackAttach, onTrackUnattach }: HeadProps) {
-  const [wasCopied, setCopied] = useState(false)
-
   const track = useReactiveVar(currentTrackVar)
-
-  useEffect(() => {
-    setCopied(false)
-  }, [track.id])
-
-  const onCopyTrackInfo = () => {
-    // INFO: it is a query permission for Blink only engines
-    // Is there a need to check permissions?
-
-    // navigator.permissions.query({ name: 'clipboard-write' as PermissionName }).then((result) => {
-    //   if (result.state == 'granted' || result.state == 'prompt') {
-    //     console.log('Write access granted!')
-    //   }
-    // })
-
-    navigator.clipboard.writeText(`${track.title} - ${track.author}`).then(
-      () => {
-        setCopied(true)
-      },
-      () => {
-        console.log('falied')
-      }
-    )
-  }
 
   return (
     <div>
@@ -64,9 +36,8 @@ export function Head({ onTrackAttach, onTrackUnattach }: HeadProps) {
               {/*  */}
               <UnattachButton onTrackUnattached={onTrackUnattach} />
               {/*  */}
-              <IconButton onClick={onCopyTrackInfo} className={styles.button}>
-                <SvgIcon>{wasCopied ? <CheckIcon /> : <CopyIcon />}</SvgIcon>
-              </IconButton>
+              <CopyInfoButton className={styles.button} />
+              {/*  */}
               <IconButton className={styles.button}>
                 <SvgIcon>
                   <ShareIcon />
