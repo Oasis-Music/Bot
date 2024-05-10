4039b1896803ce311f0bd78fe77e262216e8fd49
fresonn <vladislavs319@gmail.com>
2024-05-10 18:00:19 +0300

    client: check auth feature update

--- client/src/features/auth/check-auth.ts
+++ client/src/features/auth/check-auth.ts
@@ -1,26 +1,38 @@
-import { AuthStore, accessTokenVar } from '@/entities/auth'
 import { UserStore } from '@/entities/user'
+import { AuthStore } from '@/entities/auth'
+
+interface refreshReponse {
+  accessToken: string
+}
 
 export function checkAuth(): Promise<void> {
   return new Promise((res, rej) => {
-    const token = accessTokenVar()
-    if (token) {
-      const authData = AuthStore.signIn(token)
-      if (!authData) {
+    fetch(import.meta.env.VITE_API_URL + '/refresh', {
+      method: 'POST',
+      cache: 'no-cache',
+      credentials: 'include'
+    })
+      .then((resp) => {
+        if (!resp.ok) throw new Error(`bad request, code ${resp.status}`)
+        return resp.json()
+      })
+      .then((data: refreshReponse) => {
+        const tokenData = AuthStore.signIn(data.accessToken)
+        if (!tokenData) throw new Error('invalid access token')
+
+        UserStore.setUser({
+          id: tokenData.userId
+        })
+
+        console.log('%c authentication successfully refreshed ', 'background: #222; color: #bada55')
+        res()
+      })
+      .catch((err) => {
+        console.warn(err)
+        rej(err)
+        console.info('logout...')
         AuthStore.logout()
         UserStore.clearUser()
-        rej()
-        return
-      }
-
-      UserStore.setUser({
-        id: authData.userId
       })
-
-      res()
-    } else {
-      AuthStore.logout()
-      rej()
-    }
   })
 }
